1690812c07f88a4a8f664b5e72426eaff2daf8a8
Aryan <gitaryan1407@gmail.com>
2024-02-10 13:27:31 +0530

    initial release

---
+++ README.md
@@ -0,0 +1,14 @@
+** Technology used : **
+- Python3
+- Flask
+- chardet
+
+** GETTING STARTED : **
+
+`bash
+pip install -r requirements.txt
+`
+
+`bash
+python3 main.py
+`
--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from flask import Flask
+import chardet
+from flask import Flask,request
 app = Flask(__name__)
 
-@app.route("/<file_name>")
-def home(file_name):
-    file = open('./assets/file1.txt','r')
-    return file.read()
+@app.route("/fetch", methods=['GET'])
+def home():
+    try:
+        # Optional params       
+        file = str(request.args.get('file', "file1.txt"))
+        start = int(request.args.get('line_start', 0))
+        end = int(request.args.get('line_end', -1))
+        
+        path = f'./assets/{file}'
+        with open(path, 'rb') as file:
+            data = file.read()
+            result = chardet.detect(data)
+        
+        with open(path, 'r',encoding=result['encoding']) as file:
+            if end!= -1:
+                lines = file.readlines()[start:end]
+            else:
+                file.readlines()[start:]
+
+        html_content = '<html><body>'
+        for line in lines:
+            html_content += f'<p>{line}</p>'
+        html_content += '</body></html>'
+        return html_content
+    
+    except Exception as e:
+        html_content = '<html><body>'
+        html_content += '<h1>Error detail :: </h1>'
+        html_content += f'<p>{str(e)}</p>'
+        return html_content
+
+app.run()
